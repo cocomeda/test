83fcf44e5240179e8b5503586c156db44a484acc
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-03-14 08:38:45 +0900

    Update GAS_to_python.py

--- GAS_to_python.py
+++ GAS_to_python.py
@@ -11,5 +11,22 @@ def receive_image():
         f.write(base64.b64decode(image_data.split(',')[1]))
     return 'Image received successfully!'
 
+
+                   
+                    url = "https://script.google.com/macros/s/AKfycbx0TTHh88dW9R3wwzISZWUcHiOHcS1p58obwi2dn6yIkkNHQ_3wzomPfjeqxYwGoWDgMQ/exec"
+
+                    # JSON形式でデータを用意してdataに格納
+                    data = {
+                    "class": 'a',
+                    "confidence": 'b',
+                    "bounding_box" : 'b'
+                    }
+                    # json.dumpでデータをJSON形式として扱う
+                    r = requests.post(url, data=json.dumps(data))
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
